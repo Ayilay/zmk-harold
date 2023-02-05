0ec10d02127ffc428fa5d92bde8a4b694ad1b065
Ayilay <georgetroulis@gmail.com>
2023-02-05 13:47:11 -0800

    Main is f**ked, backport it here and fix everything

---
+++ config/compile_local.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Uses local copy of ZMK to compile keyboard. It's faster
+
+
+if [ $# -gt 0 ]; then
+    echo "usage: $0 $1"
+    exit 1
+fi
+
+echo "building harold keyboard, plz wait..."
+
+localdir=$(pwd)
+zmkconf="/home/ayilay/downloads/zmk-harold/config"
+zmkapp="/home/ayilay/downloads/zmk/app"
+zmkbuild="${zmkapp}/build"
+
+cd $zmkapp
+west build -d build/left -b seeeduino_xiao_ble -- -DSHIELD=harold_left -DZMK_CONFIG=$zmkconf
+west build -d build/right -b seeeduino_xiao_ble -- -DSHIELD=harold_right -DZMK_CONFIG=$zmkconf
+
+cd $localdir
+cp ${zmkbuild}/right/zephyr/zmk.uf2 ./builds/harold_right.uf2
+cp ${zmkbuild}/left/zephyr/zmk.uf2 ./builds/harold_left.uf2
